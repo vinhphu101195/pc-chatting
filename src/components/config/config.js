51edd4e586df859055ec1369035a1d4c7b5ba030
PhuChau <vinhphu101195@gmail.com>
2019-04-20 15:12:12 +0300

    new update

---
+++ src/components/config/config.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyDR2YNy-GGUHiex_wKjKVvdfR6lEOxScvQ",
+  authDomain: "chatting-45431.firebaseapp.com",
+  databaseURL: "https://chatting-45431.firebaseio.com",
+  projectId: "chatting-45431",
+  storageBucket: "chatting-45431.appspot.com",
+  messagingSenderId: "753331631541"
+};
+export default config;
