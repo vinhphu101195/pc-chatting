c88e42153bd14e82fe29105fca1a3fe22878e6d4
PhuChau <vinhphu101195@gmail.com>
2019-04-21 23:41:16 +0300

    conect firebase to react

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,24 @@
+// import firebase, { app } from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+import app from "firebase/app";
+
+var config = {
+  apiKey: "AIzaSyDR2YNy-GGUHiex_wKjKVvdfR6lEOxScvQ",
+  authDomain: "chatting-45431.firebaseapp.com",
+  databaseURL: "https://chatting-45431.firebaseio.com",
+  projectId: "chatting-45431",
+  storageBucket: "chatting-45431.appspot.com",
+  messagingSenderId: "753331631541"
+};
+
+// firebase.initializeApp(config);
+// firebase.firestore().settings({ timestampsInSnapshots: true });
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+  }
+}
+
+export default Firebase;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,55 +1,93 @@
 import React, { Component } from "react";
 import * as firebase from "firebase";
 import profilePic from "./images/profile_placeholder.png";
-import config from "./config/config";
+import { FirebaseContext } from "./Firebase";
 
 export default class navbar extends Component {
+  state = {
+    trachAuth: false,
+    name: "",
+    pic: ""
+  };
   signIn = () => {
-    firebase.initializeApp(config);
-
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider);
+    this.setState({ trachAuth: true });
   };
 
   signOut = () => {
-    firebase.initializeApp(config);
-
     firebase.auth().signOut();
+    this.setState({ trachAuth: false });
+  };
+
+  initFirebaseAuth = () => {
+    firebase.auth().onAuthStateChanged(this.authStateObserver);
+  };
+
+  authStateObserver = user => {
+    if (user) {
+      this.setState({
+        trachAuth: true,
+        name: this.getUserName(),
+        pic: this.getProfilePicUrl()
+      });
+    }
+  };
+
+  getUserName = () => {
+    return firebase.auth().currentUser.displayName;
+  };
+
+  getProfilePicUrl = () => {
+    return firebase.auth().currentUser.photoURL || profilePic;
+  };
+
+  isUserSignedIn = () => {
+    return !!firebase.auth().currentUser;
   };
 
   render() {
-    console.log(config);
+    console.log();
 
     return (
-      <header className="mdl-layout__header mdl-color-text--white mdl-color--light-blue-700">
-        <div className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">
-          <div className="mdl-layout__header-row mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-cell--12-col-desktop">
-            <h3>
-              <i className="material-icons">chat_bubble_outline</i> PC Chatting
-            </h3>
-          </div>
-          <div id="user-container">
-            <div hidden id="user-pic" />
-            <div hidden id="user-name" />
-            <button
-              hidden
-              id="sign-out"
-              className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--white"
-              onClick={() => this.signOut()}
-            >
-              Sign-out
-            </button>
-            <button
-              id="sign-in"
-              className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--white"
-              onClick={() => this.signIn()}
-            >
-              <i className="material-icons">account_circle</i>Sign-in with
-              Google
-            </button>
-          </div>
-        </div>
-      </header>
+      <FirebaseContext.Consumer>
+        {firebase => {
+          return (
+            <header className="mdl-layout__header mdl-color-text--white mdl-color--light-blue-700">
+              <div className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">
+                <div className="mdl-layout__header-row mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-cell--12-col-desktop">
+                  <h3>
+                    <i className="material-icons">chat_bubble_outline</i> PC
+                    Chatting
+                  </h3>
+                </div>
+                <div id="user-container">
+                  <div hidden id="user-pic" />
+                  <div hidden id="user-name" />
+                  {this.state.trachAuth === false ? (
+                    <button
+                      id="sign-in"
+                      className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--white"
+                      onClick={() => this.signIn()}
+                    >
+                      <i className="material-icons">account_circle</i>Sign-in
+                      with Google
+                    </button>
+                  ) : (
+                    <button
+                      id="sign-out"
+                      className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--white"
+                      onClick={() => this.signOut()}
+                    >
+                      Sign-out
+                    </button>
+                  )}
+                </div>
+              </div>
+            </header>
+          );
+        }}
+      </FirebaseContext.Consumer>
     );
   }
 }
