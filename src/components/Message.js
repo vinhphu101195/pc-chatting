c0d50a6f5da5766167323d9ccd84ac30332a6c8f
PhuChau <vinhphu101195@gmail.com>
2019-04-26 00:00:12 +0300

    fix

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,6 +4,7 @@ export default function Message(props) {
   console.log(props.message);
 
   return (
+    <div className="message-container">
       <div className="spacing">
         <div className="pic" />
       </div>
