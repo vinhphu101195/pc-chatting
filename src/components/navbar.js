20e4e05340b762a7aab78bd32d1758e79432392d
PhuChau <vinhphu101195@gmail.com>
2019-10-20 15:19:27 +0300

    edit firebase in gitignore and color from navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -37,7 +37,7 @@ class navbar extends Component {
 
   render() {
     return (
-      <header className="mdl-layout__header mdl-color-text--white mdl-color--light-blue-700">
+      <header className="mdl-layout__header mdl-color-text--white mdl-color--light-grey-700">
         <div className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">
           <div className="mdl-layout__header-row mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-cell--12-col-desktop">
             <h3>
