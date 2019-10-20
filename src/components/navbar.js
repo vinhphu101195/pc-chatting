b73aeb4885ac635a5f2f47b38bd2c84ea0b61d09
PhuChau <vinhphu101195@gmail.com>
2019-10-20 14:56:52 +0300

    change navbar color

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -37,7 +37,7 @@ class navbar extends Component {
 
   render() {
     return (
-      <header className="mdl-layout__header mdl-color-text--white mdl-color--light-blue-700">
+      <header className="mdl-layout__header mdl-color-text--white mdl-color--light-yellow-700">
         <div className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">
           <div className="mdl-layout__header-row mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-cell--12-col-desktop">
             <h3>
@@ -49,8 +49,8 @@ class navbar extends Component {
               {this.state.authUser != null ? (
                 <img className="auth-img" src={this.state.img} />
               ) : (
-                ""
-              )}
+                  ""
+                )}
             </div>
             <div id="user-name">
               {this.state.authUser != null ? this.state.name : ""}
@@ -64,15 +64,15 @@ class navbar extends Component {
                 Sign-out
               </button>
             ) : (
-              <button
-                id="sign-in"
-                className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--white"
-                onClick={event => this.SignIn(event)}
-              >
-                <i className="material-icons">account_circle</i>Sign-in with
-                Google
+                <button
+                  id="sign-in"
+                  className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--white"
+                  onClick={event => this.SignIn(event)}
+                >
+                  <i className="material-icons">account_circle</i>Sign-in with
+                  Google
               </button>
-            )}
+              )}
             <button
               hidden
               id="sign-out"
